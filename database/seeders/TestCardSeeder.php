a0ad733011080d64755bf0d3131f78bf113407c7
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2026-01-31 07:22:45 +0300

    feat: update TestCardSeeder with English translations for card details and error messages

--- database/seeders/TestCardSeeder.php
+++ database/seeders/TestCardSeeder.php
@@ -10,7 +10,7 @@ class TestCardSeeder extends Seeder
     public function run(): void
     {
         $cards = [
-            // --- BAŞARILI KARTLAR ---
+            // --- SUCCESSFUL CARDS ---
             ['card_number' => '5890040000000016', 'bank_name' => 'Akbank', 'scheme' => 'Master Card', 'type' => 'Debit', 'should_succeed' => true],
             ['card_number' => '5526080000000006', 'bank_name' => 'Akbank', 'scheme' => 'Master Card', 'type' => 'Credit', 'should_succeed' => true],
             ['card_number' => '9792072000017956', 'bank_name' => 'Akbank', 'scheme' => 'Troy', 'type' => 'Credit', 'should_succeed' => true],
@@ -24,43 +24,43 @@ class TestCardSeeder extends Seeder
             ['card_number' => '374427000000003',  'bank_name' => 'Garanti', 'scheme' => 'Amex', 'type' => 'Credit', 'should_succeed' => true],
             ['card_number' => '4475050000000003', 'bank_name' => 'Halkbank', 'scheme' => 'Visa', 'type' => 'Debit', 'should_succeed' => true],
             ['card_number' => '5528790000000008', 'bank_name' => 'Halkbank', 'scheme' => 'Master Card', 'type' => 'Credit', 'should_succeed' => true],
-            ['card_number' => '5892830000000000', 'bank_name' => 'İş Bankası', 'scheme' => 'Master Card', 'type' => 'Debit', 'should_succeed' => true],
-            ['card_number' => '4543590000000006', 'bank_name' => 'İş Bankası', 'scheme' => 'Visa', 'type' => 'Credit', 'should_succeed' => true],
-            ['card_number' => '6500528865390837', 'bank_name' => 'Vakıfbank', 'scheme' => 'Troy', 'type' => 'Debit', 'should_succeed' => true],
-            ['card_number' => '6501700194147183', 'bank_name' => 'Vakıfbank', 'scheme' => 'Troy', 'type' => 'Credit', 'should_succeed' => true],
-            ['card_number' => '5168880000000002', 'bank_name' => 'Yapı Kredi', 'scheme' => 'Master Card', 'type' => 'Debit', 'should_succeed' => true],
-            ['card_number' => '5451030000000000', 'bank_name' => 'Yapı Kredi', 'scheme' => 'Master Card', 'type' => 'Credit', 'should_succeed' => true],
+            ['card_number' => '5892830000000000', 'bank_name' => 'Is Bankasi', 'scheme' => 'Master Card', 'type' => 'Debit', 'should_succeed' => true],
+            ['card_number' => '4543590000000006', 'bank_name' => 'Is Bankasi', 'scheme' => 'Visa', 'type' => 'Credit', 'should_succeed' => true],
+            ['card_number' => '6500528865390837', 'bank_name' => 'Vakifbank', 'scheme' => 'Troy', 'type' => 'Debit', 'should_succeed' => true],
+            ['card_number' => '6501700194147183', 'bank_name' => 'Vakifbank', 'scheme' => 'Troy', 'type' => 'Credit', 'should_succeed' => true],
+            ['card_number' => '5168880000000002', 'bank_name' => 'Yapi Kredi', 'scheme' => 'Master Card', 'type' => 'Debit', 'should_succeed' => true],
+            ['card_number' => '5451030000000000', 'bank_name' => 'Yapi Kredi', 'scheme' => 'Master Card', 'type' => 'Credit', 'should_succeed' => true],
 
-            // --- HATALI KARTLAR ---
+            // --- ERROR/FAILURE CARDS ---
             [
                 'card_number' => '4111111111111129',
                 'should_succeed' => false,
                 'error_code' => '50064',
-                'error_message' => 'Yetersiz Bakiye (Not sufficient funds)'
+                'error_message' => 'Insufficient Balance'
             ],
             [
                 'card_number' => '4129111111111111',
                 'should_succeed' => false,
                 'error_code' => '50005',
-                'error_message' => 'İşlem Onaylanmadı (Do not honour)'
+                'error_message' => 'Transaction Not Approved (Do not honor)'
             ],
             [
                 'card_number' => '4126111111111114',
                 'should_succeed' => false,
                 'error_code' => '50018',
-                'error_message' => 'Çalıntı Kart (Stolen card)'
+                'error_message' => 'Stolen Card'
             ],
             [
                 'card_number' => '4125111111111115',
                 'should_succeed' => false,
                 'error_code' => '50073',
-                'error_message' => 'Kartın süresi dolmuş (Expired card)'
+                'error_message' => 'Expired Card'
             ],
             [
                 'card_number' => '4121111111111119',
                 'should_succeed' => false,
                 'error_code' => '50099',
-                'error_message' => 'Fraud Şüphesi'
+                'error_message' => 'Fraud Suspect'
             ],
         ];
 
